cc7ee2b723d16972c7eceafb53c838ae3dee4e65
Andreas Lotarev <andreas.lotarev@outlook.com>
2025-05-12 19:55:46 +0200

    debug deploy

--- Banebooking.Api/ClientApp/vite.config.ts
+++ Banebooking.Api/ClientApp/vite.config.ts
@@ -13,3 +13,7 @@ export default defineConfig({
         }
     }
 })
+
+console.log('VITE_SUPABASE_URL:', process.env.VITE_SUPABASE_URL);
+console.log('VITE_SUPABASE_ANON_KEY:', process.env.VITE_SUPABASE_ANON_KEY);
+
