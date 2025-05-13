bc21ddbc1d91a234a27806048bf16038dc4189c5
Andreas Lotarev <andreas.lotarev@outlook.com>
2025-05-13 12:19:25 +0200

    nu da

--- Banebooking.Api/ClientApp/src/supabase.ts
+++ Banebooking.Api/ClientApp/src/supabase.ts
@@ -1,13 +1,7 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from '@supabase/supabase-js'
 
-const url = import.meta.env.VITE_SUPABASE_URL
-const key = import.meta.env.VITE_SUPABASE_ANON_KEY
-
-console.log('SUPABASE URL:', url)
-console.log('SUPABASE KEY:', key)
-
-if (!url || !key) {
-  throw new Error('Supabase URL eller ANON_KEY mangler – sjekk miljøvariabler.')
-}
+// TODO: Bytt til import.meta.env.VITE_SUPABASE_URL osv. når miljøvariabler fungerer i deploy
+const url = 'https://wfklkakrogxbhozxhtqd.supabase.co'
+const key = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indma2xrYWtyb2d4YmhvenhodHFkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY5MDk0MjEsImV4cCI6MjA2MjQ4NTQyMX0.RK4dQ05OSiU_88dOi4qakHKoL0_zpUuZjAN9au57KIE'
 
 export const supabase = createClient(url, key)
--- Banebooking.Api/ClientApp/vite.config.ts
+++ Banebooking.Api/ClientApp/vite.config.ts
@@ -1,9 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-console.log('VITE_SUPABASE_URL =', process.env.VITE_SUPABASE_URL);
-console.log('VITE_SUPABASE_ANON_KEY =', process.env.VITE_SUPABASE_ANON_KEY);
-
 export default defineConfig({
     plugins: [react()],
     server: {
