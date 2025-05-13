3b57931fca1d467bd442acac4fefff8bb9f21f74
Andreas Lotarev <andreas.lotarev@outlook.com>
2025-05-13 09:59:34 +0200

    mere feilsøk

--- Banebooking.Api/ClientApp/vite.config.ts
+++ Banebooking.Api/ClientApp/vite.config.ts
@@ -11,5 +11,9 @@ export default defineConfig({
                 secure: false
             }
         }
+    },
+    define: {
+        'import.meta.env.VITE_SUPABASE_URL': JSON.stringify(process.env.VITE_SUPABASE_URL),
+        'import.meta.env.VITE_SUPABASE_ANON_KEY': JSON.stringify(process.env.VITE_SUPABASE_ANON_KEY),
     }
 })
