e23d60e01969ee7a7b3e0554aa1e438a0b2e4533
Andreas Lotarev <andreas.lotarev@outlook.com>
2025-05-13 09:21:34 +0200

    fjenet log i vite config

--- Banebooking.Api/ClientApp/vite.config.ts
+++ Banebooking.Api/ClientApp/vite.config.ts
@@ -13,7 +13,3 @@ export default defineConfig({
         }
     }
 })
-
-console.log('VITE_SUPABASE_URL:', import.meta.env.VITE_SUPABASE_URL);
-console.log('VITE_SUPABASE_ANON_KEY:', import.meta.env.VITE_SUPABASE_ANON_KEY);
-
