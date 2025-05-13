079d349e0bc29b737ea18b2286cddf0ba50cf92f
Andreas Lotarev <andreas.lotarev@outlook.com>
2025-05-13 10:08:17 +0200

    mere feilsøk

--- Banebooking.Api/ClientApp/src/supabase.ts
+++ Banebooking.Api/ClientApp/src/supabase.ts
@@ -1,10 +1,11 @@
-import { createClient } from '@supabase/supabase-js';
+const url = import.meta.env.VITE_SUPABASE_URL
+const key = import.meta.env.VITE_SUPABASE_ANON_KEY
 
-const url = import.meta.env.VITE_SUPABASE_URL;
-const key = import.meta.env.VITE_SUPABASE_ANON_KEY;
+console.log('SUPABASE URL:', url)
+console.log('SUPABASE KEY:', key)
 
 if (!url || !key) {
-    throw new Error('Supabase URL eller ANON_KEY mangler – sjekk miljøvariabler.');
+  throw new Error('Supabase URL eller ANON_KEY mangler – sjekk miljøvariabler.')
 }
 
-export const supabase = createClient(url, key);
+export const supabase = createClient(url, key)
--- Banebooking.Api/ClientApp/vite.config.ts
+++ Banebooking.Api/ClientApp/vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
         }
     },
     define: {
-        'import.meta.env.VITE_SUPABASE_URL': JSON.stringify(process.env.VITE_SUPABASE_URL),
-        'import.meta.env.VITE_SUPABASE_ANON_KEY': JSON.stringify(process.env.VITE_SUPABASE_ANON_KEY),
+        'import.meta.env.VITE_SUPABASE_URL': JSON.stringify(process.env.VITE_SUPABASE_URL ?? ''),
+		'import.meta.env.VITE_SUPABASE_ANON_KEY': JSON.stringify(process.env.VITE_SUPABASE_ANON_KEY ?? ''),
     }
 })
