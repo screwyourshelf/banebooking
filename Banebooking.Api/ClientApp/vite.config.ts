74c37db58b21a565169b50e5a7558b2b82153e94
Andreas Lotarev <andreas.lotarev@outlook.com>
2025-05-13 10:57:06 +0200

    tester hardkodet verdi

--- Banebooking.Api/ClientApp/vite.config.ts
+++ Banebooking.Api/ClientApp/vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
         }
     },
     define: {
-        'import.meta.env.VITE_SUPABASE_URL': JSON.stringify(process.env.VITE_SUPABASE_URL ?? ''),
-		'import.meta.env.VITE_SUPABASE_ANON_KEY': JSON.stringify(process.env.VITE_SUPABASE_ANON_KEY ?? ''),
+        'import.meta.env.VITE_SUPABASE_URL': JSON.stringify('https://wfklkakrogxbhozxhtqd.supabase.co'),
+		'import.meta.env.VITE_SUPABASE_ANON_KEY': JSON.stringify('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indma2xrYWtyb2d4YmhvenhodHFkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY5MDk0MjEsImV4cCI6MjA2MjQ4NTQyMX0.RK4dQ05OSiU_88dOi4qakHKoL0_zpUuZjAN9au57KIE'),
     }
 })
