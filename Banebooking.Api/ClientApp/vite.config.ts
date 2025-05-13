7019d9de955cabff60edafab282728806e92ffff
Andreas Lotarev <andreas.lotarev@outlook.com>
2025-05-13 11:45:42 +0200

    adsd

--- Banebooking.Api/ClientApp/vite.config.ts
+++ Banebooking.Api/ClientApp/vite.config.ts
@@ -14,9 +14,5 @@ export default defineConfig({
                 secure: false
             }
         }
-    },
-    define: {
-	  'import.meta.env.VITE_SUPABASE_URL': JSON.stringify(process.env.VITE_SUPABASE_URL ?? ''),
-	  'import.meta.env.VITE_SUPABASE_ANON_KEY': JSON.stringify(process.env.VITE_SUPABASE_ANON_KEY ?? ''),
-	}
+    }
 })
